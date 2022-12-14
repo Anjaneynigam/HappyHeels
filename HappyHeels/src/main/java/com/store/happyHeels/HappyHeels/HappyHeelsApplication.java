f02f465abc18f5711c42a8ad3c2f430a9847d5e4
Anjaney Nigam <nigam.anjaney77@gmail.com>
2022-12-14 18:43:28 +0530

    Initial commit

---
+++ HappyHeels/src/main/java/com/store/happyHeels/HappyHeels/HappyHeelsApplication.java
@@ -0,0 +1,14 @@
+package com.store.happyHeels.HappyHeels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HappyHeelsApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyHeelsApplication.class, args);
+	}
+
+}
